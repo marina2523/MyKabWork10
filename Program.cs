87392a95d4f1d6326db89a725f6a698dc34132cc
Marina <114664502+marina2523@users.noreply.github.com>
2022-11-18 19:27:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace EX1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double z, x, n;
+            z = 0;
+            Console.Write("x= ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("(n>0) n = ");
+            n = double.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++) z += Math.Cos(x) + ((Math.Cos(3 * x)) / 9) + ((Math.Cos(5 * x)) / 25);
+            Console.WriteLine("z = {0}", z);
+        }
+    }
+}
+
